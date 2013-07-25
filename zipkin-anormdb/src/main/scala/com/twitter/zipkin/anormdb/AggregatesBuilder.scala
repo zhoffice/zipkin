9f3244e647a6b5bf1b9ec0e39ec2ec41af959f6e
Isaac Sukin <isukin@twitter.com>
2013-07-25 16:20:00 -0700

    Starting on aggregates stuff

---
+++ zipkin-anormdb/src/main/scala/com/twitter/zipkin/anormdb/AggregatesBuilder.scala
@@ -0,0 +1,12 @@
+package main.scala.com.twitter.zipkin.anormdb
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: isulkin
+ * Date: 7/25/13
+ * Time: 1:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class AggregatesBuilder {
+
+}
---
+++ zipkin-anormdb/src/main/scala/com/twitter/zipkin/storage/anormdb/AnormAggregates.scala
@@ -0,0 +1,12 @@
+package main.scala.com.twitter.zipkin.storage.anormdb
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: isulkin
+ * Date: 7/25/13
+ * Time: 1:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class AnormAggregates {
+
+}
